e74334a06f355c76fb7e372c669d297543e50470
聂元培 <nieyuanpei@qutoutiao.net>
2020-04-09 13:01:49 +0800

    添加转int64支持

--- int.go
+++ int.go
@@ -1,8 +1,29 @@
 package gtype
 
-import "strconv"
+import (
+	"errors"
+	"reflect"
+	"strconv"
+)
 
 func Int64ToString(v int64) string {
 	s := strconv.FormatInt(v, 10)
 	return s
+}
+func toInt64(v interface{}) (int64, error) {
+	var r int64
+	r = 0
+	switch reflect.TypeOf(v).Kind() {
+	case reflect.Int64:
+		r = v.(int64)
+		break
+	case reflect.String:
+		s := v.(string)
+		r, _ = strconv.ParseInt(s, 10, 64)
+		break
+	default:
+		return 0, errors.New("not found relation type")
+	}
+
+	return r, nil
 }
